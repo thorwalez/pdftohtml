50d10479165776c14ba43c5f1bf20458e3d2da77
Mike Hartl <mike_hartl@gmx.de>
2021-04-09 12:51:17 +0200

    supported postscript converter

---
+++ src/Converter/GermanSpecialCharacterToPostScriptCharacter.php
@@ -0,0 +1,43 @@
+<?php
+
+
+namespace ThorWalez\PdfToHtml\Converter;
+
+/**
+ * Class GermanSpecialCharacterToPostScriptCharacter
+ * @package ThorWalez\PdfToHtml\Converter
+ */
+class GermanSpecialCharacterToPostScriptCharacter
+{
+    const MAP_CHARACTER_LIST = [
+        '\201' => 'ü',
+        '\232' => 'Ü',
+        '\204' => 'ä',
+        '\216' => 'Ä',
+        '\224' => 'ö',
+        '\231' => 'Ö',
+        '\341' => 'ß',
+        //            '\304' =>'Ä',
+        //            '\344' =>'ä',
+        //            '\334' =>'Ü',
+        //            '\374' =>'ü',
+        //            '\326' =>'Ö',
+        //            '\366' =>'ö',
+    ];
+
+    /**
+     * @param string $input
+     * @return string
+     */
+    public function convert(string $input) : string
+    {
+        foreach (self::MAP_CHARACTER_LIST as $hit => $gsc) {
+            if (\strpos($input, $gsc) !== false) {
+                $input = \str_replace($gsc, $hit, $input);
+            }
+        }
+
+        return $input;
+    }
+
+}
